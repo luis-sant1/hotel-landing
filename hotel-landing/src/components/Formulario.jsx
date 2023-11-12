3554206148339672d237ca9485199fbe568085d8
JorgeAzuaje <jorgeantonioazuajevilla@gmail.com>
2023-11-12 00:54:32 -0400

    Añadi diseños al formulario

--- hotel-landing/src/components/Formulario.jsx
+++ hotel-landing/src/components/Formulario.jsx
@@ -1,27 +1,72 @@
 import {useForm} from  'react-hook-form'
+
 export default function Formulario () {
     return ( 
-        <form className='bg-red-500'>
-            <label htmlFor="">Nombre</label>
-            <input type="text" className='w-10 pr-10' />
-            <label htmlFor="">Primer Apellido</label>
-            <input type="text" />
-            <label htmlFor="">Correo Electronico</label>
-            <input type="text" />
-            <label htmlFor="">Fecha de entrada</label>
-            <input type="date" />
-            <label htmlFor="">Fecha de salida</label>
-            <input type="date" />
-            <label htmlFor="">Número de teléfono</label>
-            <input type="text" />
-            <label htmlFor="">Dirección</label>
-            <input type="text" />
-            <label htmlFor="">Fecha de salida</label>
-            <select name="" id="">
-                <option value="">Habitación 1</option>
-                <option value="">Habitación 2</option>
-                <option value="">Habitación 3</option>
-            </select>
-        </form>
+        
+        <div className='h-full'>
+        <div className='mr-auto ml-auto w-11/12 md:w-7/12 lg:w-3/12'>
+            <form className='grid grid-cols-1 justify-center h-full'>
+                <label htmlFor="" className='font-light pt-10'>Nombre</label>
+                <div className='pt-1'>
+                <input type="text"  className='font-light w-full border border-solid border-black grid h-10 p-2'/>
+                </div>
+                
+                <label htmlFor="" className='font-light pt-2'>Primer Apellido</label>
+                <div className='pt-1'>
+                    <input type="text" className='font-light w-full border border-solid border-black grid h-10 p-2'/>
+                </div>
+                <label htmlFor="" className='font-light pt-2'>Correo Electronico</label>
+                <div className='pt-1'>
+                    <input type="text" className='font-light w-full border border-solid border-black grid h-10 p-2'/>
+                </div>
+                <div className='pt-1'>
+                    <div className='grid grid-cols-2 pt-1 justify-items-center'>
+                        <label htmlFor="" className='font-light'>Fecha de entrada</label>
+                        <label htmlFor="" className='font-light'>Fecha de salida</label>
+                    </div>
+                    
+                    <div className='grid grid-cols-2 justify-items-center'>
+                        <input type="date" className='font-light h-10 w-8/12 border border-solid border-black p-2'/>
+                        <input type="date" className='font-light h-10 w-8/12 border border-solid border-black p-2'/>
+                    </div>
+                    
+                </div>
+                
+
+                <label htmlFor="" className='font-light pt-2'>Número de teléfono</label>
+                <div className='pt-1'>
+                    <input type="text" className='font-light w-full border border-solid border-black grid h-10 p-2'/>
+                </div>
+                
+                <label htmlFor="" className='font-light pt-2'>Dirección</label>
+                <div className='pt-1'>
+                    <input type="text" className='font-light w-full border border-solid border-black grid h-10 p-2'/>
+                </div>
+                
+                <label htmlFor="" className='font-light pt-2'>Fecha de salida</label>
+                <div className='pt-1'>
+                    <select name="" id="" className='font-light h-10 w-full border border-solid border-black p-2'>
+                        <option value="" className='font-light'>Habitación 1</option>
+                        <option value="" className='font-light'>Habitación 2</option>
+                        <option value="" className='font-light'>Habitación 3</option>
+                    </select>
+                </div>
+                <div className='flex pt-4'>
+                    <input type="checkbox" className='h-5 w-5'/>
+                    <div className=''>
+                        <label htmlFor="" className='font-light pl-2'>
+                            Acepto los terminos y condiciones
+                        </label>
+                    </div>
+                </div>
+                <div className='pt-3 grid justify-items-center'>
+                    <button className='font-light bg-yellow-800 w-32 h-10 text-white text-2xl'>Reserva</button>
+                </div>
+            </form>
+        </div>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap');
+        </style>
+        </div>
     )
 }
