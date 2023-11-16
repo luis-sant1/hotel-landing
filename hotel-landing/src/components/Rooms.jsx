a48860def6567672a62bd5f64c8dbe4fa2839b09
luis-sant1 <santiagomuchacholuma@uvm.edu.ve>
2023-11-15 23:27:55 -0600

    ADD Handle to see more. Work in progress.

--- hotel-landing/src/components/Rooms.jsx
+++ hotel-landing/src/components/Rooms.jsx
@@ -1,6 +1,15 @@
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2"
 
 export default function Rooms (props){
+    const handleSeeMore = () => {
+        Swal.fire({
+            title: "¡Página aun no disponible!",
+            text: "Página en desarrollo, de momento. Gracias.",
+            icon: "warning",
+            confirmButtonColor: "#9A5832"
+        });
+    }
     return(
         <div className="m-0 w-full h-full group  flex text-center ">
             <img 
@@ -9,6 +18,7 @@ export default function Rooms (props){
             <div className="absolute z-10 hidden group-hover:block m-auto left-0 right-0  top-[40%] text-white " >
                 <h2 className="text-xl font-light">{props.type}</h2>
                 <Link to='#'
+                onClick={handleSeeMore}
                 className=""><span className="text-lg relative after:bg-white after:absolute after:h-0.5 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer">Ver mas</span></Link>
                 <p className="font-light">{props.promo}</p>
             </div>
