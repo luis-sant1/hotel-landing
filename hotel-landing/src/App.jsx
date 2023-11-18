fd6df6726b48d7e568e5d23a88d48b372c7d9c41
JorgeAzuaje <jorgeantonioazuajevilla@gmail.com>
2023-11-18 18:45:08 -0400

    Cree varios componentes
    Se crearon varios componentes con formularios entre los que estan, para editar y crear trajetas de descubre nuestras playas, para crear testimonios, y para editar datos sobre las habitaciones.

--- hotel-landing/src/App.jsx
+++ hotel-landing/src/App.jsx
@@ -4,6 +4,10 @@ import NavBar from './components/NavBar'
 import Formulario from './components/Form.jsx'
 import ScrollToHashElement from './components/utils/ScrollToHashElement.js'
 import { AuthProvider } from './components/context/AuthContext.jsx'
+import EditPlaForm from './components/EditPlaForm.jsx'
+import CreatePlaForm from './components/CreatePlaForm.jsx'
+import CreateReview from './components/CreateReview.jsx'
+import EditRooms from './components/EditRooms.jsx'
 
 function App() {
   return (
@@ -14,6 +18,11 @@ function App() {
           <Routes>
             <Route path='/*' element={<Landing />} />
             <Route path='/reservation-form' element={<Formulario/>} />
+            <Route path='/edit-pla-form' element={<EditPlaForm/>} />
+            <Route path='/create-pla-form' element={<CreatePlaForm/>} />
+            <Route path='/create-review' element={<CreateReview/>} />
+            <Route path='/edit-rooms' element={<EditRooms/>} />
+            
            
           </Routes>
         </BrowserRouter>
