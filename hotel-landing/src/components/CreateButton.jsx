71d6199ab38eef76d7e71039a6cba79c4f7d669d
JorgeAzuaje <jorgeantonioazuajevilla@gmail.com>
2023-11-18 21:54:32 -0400

    Cree boton de crear abitacion

---
+++ hotel-landing/src/components/CreateButton.jsx
@@ -0,0 +1,13 @@
+export default function CreateButton() {
+    return(
+        <div>
+            <p className="font-light text-3xl text-center pb-5 mb:text-4xl">Crear habitación</p>
+            <div className="flex justify-center">
+                
+                <div className='pt-5 grid justify-items-center '>
+                    <button type="" className='font-light bg-yellow-800 w-32 h-10 text-white text-2xl'>Crear</button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- hotel-landing/src/components/DisRooms.jsx
+++ hotel-landing/src/components/DisRooms.jsx
@@ -1,15 +1,19 @@
 import CarouselRooms from "./CarouselRooms"
+import CreateButton from "./CreateButton"
 export default function DisRooms() {
     return(
-        <div className='flex w-full bg-[rgba(230,230,230,1)]' >
-            <div className='w-9/12  m-auto pt-10 pb-10 md:pt-16' id= "rooms">
-                <h1 className='font-extralight pb-10 text-4xl md:text-6xl lg:text-7xl'>Habitaciones disponibles</h1>
-                <CarouselRooms />
+        <div className="bg-[rgba(230,230,230,1)]">
+            <div className='flex w-full bg-[rgba(230,230,230,1)]' >
+                <div className='w-9/12  m-auto pt-10 pb-10 md:pt-16' id= "rooms">
+                    <h1 className='font-extralight pb-10 text-4xl md:text-6xl lg:text-7xl'>Habitaciones disponibles</h1>
+                    <CarouselRooms />
+                </div>
+                
+                <style>
+                    @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap');
+                </style>
             </div>
-
-            <style>
-                @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap');
-            </style>
+        <CreateButton/>
         </div>
     )
 }
