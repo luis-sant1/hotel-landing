e8670522235b837d633008a29418f2a45ee38bb1
JorgeAzuaje <jorgeantonioazuajevilla@gmail.com>
2023-11-27 02:27:52 -0400

    commit

--- hotel-landing/src/components/Login.jsx
+++ hotel-landing/src/components/Login.jsx
@@ -73,7 +73,7 @@ export default function Iniciarsesion({ toPage, setPage }) {
 
                     }
 
-                    <divclassName='pt-10 grid justify-items-center mb-4 pb-32'>
+                    <div className='pt-10 grid justify-items-center mb-4 pb-32'>
 
                         <button type="submit" className="font-light bg-yellow-800 w-32 h-10 text-white text-2xl">
                             Iniciar Sesión
