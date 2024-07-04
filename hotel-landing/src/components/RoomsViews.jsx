883789bee3262beb8383a9a6b205299f8e19ed70
luis-sant1 <santiagomuchacholuma@uvm.edu.ve>
2024-07-03 18:57:20 -0600

    FIX Rendering alts images

--- hotel-landing/src/components/RoomsViews.jsx
+++ hotel-landing/src/components/RoomsViews.jsx
@@ -44,8 +44,7 @@ export default function RoomsViews() {
         room();
     }, [])
     const { id } = useParams()
-    console.log(room.image)
-
+    console.log(room.image?.alts?.[0])
     return (
         <div className='w-full h-full dark:bg-gray-800  bg-white'>
             <div className="m-0 w-full h-full group  flex text-center">
@@ -67,10 +66,10 @@ export default function RoomsViews() {
             <div className='lg:flex lg:pt-7'>
                 <div className="w-11/12 mr-auto ml-auto pt-7 border-b border-black lg:border-b-0 lg:w-8/12 lg:pt-1">
 
-                    <p className="text-black font-light text-lg pb-3 md:pb-6 lg:pl-8 dark:text-white">{room?.description}</p>
+                    <p className="text-black font-light text-lg pb-3 md:pb-6 lg:pl-8 dark:text-white">{room.description}</p>
 
                     <div className="hidden md:hidden lg:flex lg:w-full lg:mr-0 lg:ml-0 ">
-                        <img src="https://www.momalia.com/wp-content/uploads/2019/09/diseno-habitaciones-hotel2.jpg" alt="Imagen de la habitación" className="w-full h-96 pb-3 md:h-full lg:pb-8" />
+                        <img src={room.image?.alts?.[0]} alt="Imagen de la habitación" className="w-full h-96 pb-3 md:h-full lg:pb-8" />
                     </div>
                 </div>
                 <div className="text-black pt-3 w-11/12 mr-auto ml-auto border-y border-black md:pt-6 lg:pl-14 lg:w-3/12 lg:h-64 dark:text-white">
